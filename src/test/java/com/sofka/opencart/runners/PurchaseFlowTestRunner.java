adf558bd6068105b8fd240a5f9bcd0c04940bd95
Majo Castro <majo_castro21@hotmail.com>
2026-03-30 13:57:24 -0500

    feat: add PurchaseFlowTestRunner to execute smoke and regression feature files

--- src/test/java/com/sofka/opencart/runners/PurchaseFlowTestRunner.java
+++ src/test/java/com/sofka/opencart/runners/PurchaseFlowTestRunner.java
@@ -1,25 +1,18 @@
 package com.sofka.opencart.runners;
 
-import io.cucumber.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-/**
- * Test Runner para ejecutar las pruebas E2E con Serenity BDD
- */
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com.sofka.opencart.stepdefinitions",
         plugin = {
                 "pretty",
-                "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json"
         },
-        monochrome = false,
         tags = "@smoke or @regression"
 )
 public class PurchaseFlowTestRunner {
-    // This class remains empty, it is used only as a holder for the @RunWith annotation
-    // and the @CucumberOptions to configure the test run
 }
